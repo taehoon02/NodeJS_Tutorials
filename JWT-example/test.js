43cb6a7b88d858213ed1d8717ddf9be3f46d499a
Irenebode <kth020315@naver.com>
2018-04-24 16:58:57 +0900

    nodejs-jwt-example

---
+++ JWT-example/test.js
@@ -0,0 +1,35 @@
+const header = {
+  "typ": "JWT",
+  "alg": "HS256"
+};
+
+// encode to base64
+const encodedHeader = new Buffer(JSON.stringify(header))
+                            .toString('base64')
+                            .replace('=', '');
+
+
+const payload = {
+    "iss": "velopert.com",
+    "exp": "1485270000000",
+    "https://velopert.com/jwt_claims/is_admin": true,
+    "userId": "11028373727102",
+    "username": "velopert"
+};
+
+// encode to base64
+const encodedPayload = new Buffer(JSON.stringify(payload))
+                            .toString('base64')
+                            .replace('=', '');
+
+
+const crypto = require('crypto');
+const signature = crypto.createHmac('sha256', 'secret')
+             .update(encodedHeader + '.' + encodedPayload)
+             .digest('base64')
+             .replace('=', '');
+
+
+console.log('header: ', encodedHeader);
+console.log('payload: ', encodedPayload);
+console.log('signature: ', signature);
---
+++ nodejs-jwt-example/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secret': 'SeCrEtKeYfOrHaShInG',
+    'mongodbUri': 'mongodb://kth020315:anfkzkal-02@ds255539.mlab.com:55539/jwt-example'
+}
